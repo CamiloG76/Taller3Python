014bbade2067804dcde5b421566d35f534d8b8df
CamiloG76 <juank27gm@gmail.com>
2024-11-07 17:22:06 -0500

    Delete CadenaInvertida.py

--- CadenaInvertida.py
+++
@@ -1,8 +0,0 @@
-#invertir una cadena 
-cadena =input("ingresa una cadena para invertirla\n")
-cadena_invertida = ""
-for i in cadena:
-    cadena_invertida = i + cadena_invertida
-print(f"cadena invertida: {cadena_invertida}")
-   
-
